740768248e2cf571757ce871cc2ef18cb9688963
Ken Girard <ken.girard@gmail.com>
2018-05-01 21:11:53 -0500

    Calculates the mpg using data input by user

---
+++ src/exercises/MPG.java
@@ -0,0 +1,22 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MPG {
+    public static void main(String[] args) {
+        double miles;
+        double gas_used;
+        double mpg;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("Enter miles driven: ");
+        miles = in.nextDouble();
+        System.out.println("Enter gallons of fuel used: ");
+        gas_used = in.nextDouble();
+
+        mpg = miles / gas_used;
+        System.out.println("You are getting " + mpg + " miles per gallon of fuel");
+
+    }
+}
