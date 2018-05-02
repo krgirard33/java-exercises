3a25df38c3eb4ba64014fc96f9678a61ef9f24d7
Ken Girard <ken.girard@gmail.com>
2018-05-02 00:31:59 -0500

    Created, prints out words with a length of 5 letters. Now wondering how to make it do it in a long string, break on the blank.

---
+++ src/exercises/FiveLetters.java
@@ -0,0 +1,14 @@
+package exercises;
+
+public class FiveLetters {
+    public static void main(String[] args) {
+        String[] words = new String[] {"Boy", "Woman", "Cheeses", "Chess", "Picture", "yourself", "in", "a",
+                "boat", "on", "a", "river", "With", "tangerine", "trees", "and", "marmalade", "skies"};
+
+        for (String word : words) {
+            if (word.length() == 5) {
+                System.out.println(word);
+            }
+        }
+    }
+}
