fb7ae12854c4f0dcf06341bc2d62a13688939932
Ken Girard <ken.girard@gmail.com>
2018-05-01 20:58:50 -0500

    Calculates the area of a rectangle, and gives the perimeter as well

---
+++ src/exercises/AreaRectangle.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class AreaRectangle {
+    public static void main(String[] args) {
+        double height;
+        double width;
+        double area;
+        double perimeter;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("Enter the height: ");
+        height = in.nextDouble();
+        System.out.println("Enter the width: ");
+        width = in.nextDouble();
+
+        area = height * width;
+        perimeter = (height * 2) + (width * 2);
+        System.out.println("The area of the rectangle is " + area);
+        System.out.println("The perimeter of the rectangle is " + perimeter);
+    }
+}
