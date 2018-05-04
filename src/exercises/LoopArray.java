0cbefb048f855f63ca29a3261cf627a6a87283fa
Ken Girard <ken.girard@gmail.com>
2018-05-03 23:29:41 -0500

    Created, loops through an array and prints it out, 3 different ways

---
+++ src/exercises/LoopArray.java
@@ -0,0 +1,37 @@
+package exercises;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoopArray {
+    public static void main(String[] args){
+        int array[] = {1, 1, 2, 3, 5, 8};
+        int temp;
+        ArrayList<Integer> otherList = new ArrayList<>();
+
+        /* Prints each on a new line */
+        for(int i : array) {
+            temp = i;
+            System.out.println(temp);
+        }
+
+        /* Prints each on same line */
+        for(int j : array) {
+            temp = j;
+            System.out.print(temp + ", ");
+        }
+
+        /* Adds to the otherList array, and then prints it out */
+        otherList.add(6);
+        otherList.add(7);
+        otherList.add(8);
+        otherList.add(9);
+        otherList.add(12);
+
+        for(int k : otherList) {
+            temp = k;
+            System.out.print(temp);
+        }
+    }
+}
