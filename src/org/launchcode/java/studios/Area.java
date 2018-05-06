1298c0e61abd761fa61a5ddf713086842463a116
Ken Girard <ken.girard@gmail.com>
2018-05-06 16:25:32 -0500

    Added in a while statement, and error message, about negative numbers

--- src/org/launchcode/java/studios/Area.java
+++ src/org/launchcode/java/studios/Area.java
@@ -11,13 +11,23 @@ public class Area {
         Scanner in;
 
         in = new Scanner(System.in);
-        System.out.println("Enter the radius of the circle: ");
-        radius = in.nextDouble();
-        pi = 3.14159265;
-        area = pi * (radius * radius);
-        circumference = pi*(2*radius);
-        System.out.println("The area of the circle is: " + area);
-        System.out.println("The circumference of the circle is: " + circumference);
+        radius = 0;
+        while (radius<=0) {
+            System.out.println("Enter the radius of the circle: ");
+            radius = in.nextDouble();
+            if (radius <= 0) {
+                System.out.println("Must be a positive number");
+                System.out.println("Enter the radius of the circle: ");
+                radius = in.nextDouble();
+
+            } else {
+                pi = 3.14159265;
+                area = pi * (radius * radius);
+                circumference = pi * (2 * radius);
+                System.out.println("The area of the circle is: " + area);
+                System.out.println("The circumference of the circle is: " + circumference);
+            }
+        }
 
     }
 }
