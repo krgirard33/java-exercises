bfed4aa298aeea858c802f90e4d365f76623fde8
Ken Girard <ken.girard@gmail.com>
2018-05-08 23:15:56 -0500

    Killed Map, as I never used it, and now it accepts input from a user. I guess I could put in another if statement, so if in="" words = cow, with cow being hardcoded string...

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -1,16 +1,24 @@
 package org.launchcode.java.studios;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class CountingCharacters {
     public static void main(String[] args) {
         HashMap<Character, Integer> counted = new HashMap<>();
 
-        String words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque " +
+        /* String words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque " +
                 "sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit " +
                 "amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget " +
                 "massa. Donec nec velit non ligula efficitur luctus.";
+                */
+        String words;
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.print("Please enter some words: ");
+        words = in.nextLine();
+
         char[] charactersInString = words.toLowerCase().toCharArray();
 
         for (char i : charactersInString) {
