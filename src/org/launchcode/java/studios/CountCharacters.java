29233d30c40e0092abe8695991aa2b1a593053a6
Ken Girard <ken.girard@gmail.com>
2018-05-08 22:34:22 -0500

    Created CountingCharacters, but couldn't get it to run, so I made CountCharacters, and realized I had the hashmap in the wrong place... so now it works

--- src/org/launchcode/java/studios/CountCharacters.java
+++ src/org/launchcode/java/studios/CountCharacters.java
@@ -1,30 +1,24 @@
 package org.launchcode.java.studios;
 
 import java.util.HashMap;
-import java.util.Map;
 
-public class CountingCharacters {
-    public static void main(String[] args) {
+public class CountCharacters {
+    public static void main(String[] ags) {
         HashMap<Character, Integer> counted = new HashMap<>();
-
         String words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque " +
                 "sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit " +
                 "amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget " +
                 "massa. Donec nec velit non ligula efficitur luctus.";
-        char[] charactersInString = words.toCharArray();
+        char [] charactersInString = words.toCharArray();
 
         for (char i : charactersInString) {
             if (counted.containsKey(i)) {
-                counted.put(i, counted.get(i) + 1);
-            } else {
-                counted.put(i, 1);
+                counted.put(i, counted.get(i) +1);
             }
+            else counted.put(i, 1);
         }
-
         for (char c : counted.keySet()) {
             System.out.println(c + ":" + counted.get(c));
         }
-
     }
-
 }
