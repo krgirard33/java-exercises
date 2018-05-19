ee38446cd65002baf1e49680a6408967121ebef7
Ken Girard <ken.girard@gmail.com>
2018-05-19 17:52:21 -0500

    Umm... the rest of the setters and getters. Made setStudentId private as it should never really change

--- src/org/launchcode/java/exercises/school/Student.java
+++ src/org/launchcode/java/exercises/school/Student.java
@@ -9,8 +9,28 @@ public class Student {
     public String getName() {
         return name;
     }
-
     public void setName(String aName) {
         name = aName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+    private void setStudentId(int aStudentId) {
+        studentId = aStudentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+    public void setNumberOfCredits(int aNumberOfCredits) {
+        numberOfCredits = aNumberOfCredits;
+    }
+
+    public double gpa() {
+        return gpa;
+    }
+    public void setGpa(double aGpa) {
+        gpa = aGpa;
     }
 }
