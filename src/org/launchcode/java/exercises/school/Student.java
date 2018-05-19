4dd716a0d58e1ed85582e1a4e95d630c4a7ee59a
Ken Girard <ken.girard@gmail.com>
2018-05-19 17:33:01 -0500

    Start of Class 5 Prep Exercise, so a class with setter and getter

---
+++ src/org/launchcode/java/exercises/school/Student.java
@@ -0,0 +1,16 @@
+package org.launchcode.java.exercises.school;
+
+public class Student {
+    private String name;
+    private int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String aName) {
+        name = aName;
+    }
+}
